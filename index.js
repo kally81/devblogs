a6d2c27df82f6d8f8d266666bbea0539c0c45d6e
kally81 <kally81@nate.com>
2015-10-17 16:29:18 +0900

    변경 테스트

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
+console.log('Node app is running on port', app.get('port'));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
