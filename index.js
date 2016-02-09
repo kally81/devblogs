05b4414558a014de9e43758efa77334a871a465c
gosehwan <kally81@nate.com>
2016-02-09 16:23:37 +0900

    데이터베이스 관련 소스 변경

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var pg = require('pg');
-//pg.connect(process.env.DATABASE_URL, function(err, client) {
-//  if (err) throw err;
-//  console.log('Connected to postgres! Getting schemas...');
-//  client
-//    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//    .on('row', function(row) {
-//      console.log(JSON.stringify(row));
-//    });
-//});
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
