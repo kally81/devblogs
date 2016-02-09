34ddb6a55759d69e81b03e66b08b83f85387c806
gosehwan <kally81@nate.com>
2016-02-09 16:12:59 +0900

    데이터베이스 연동

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express');
-var app = express();
+var pg = require('pg');
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 console.log('Node app is running on port', app.get('port'));
 app.use(express.static(__dirname + '/public'));
