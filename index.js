9f55949d95a98ae041a682dfcdf38f80ca5f7bc2
gosehwan <kally81@nate.com>
2016-02-09 14:45:38 +0900

    폴리머 추

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 console.log('Node app is running on port', app.get('port'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components/'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -11,6 +12,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/index', function(request, response) {
+	  response.render('pages/test');
 });
 
 app.listen(app.get('port'), function() {
