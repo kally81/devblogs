8439d79995e1ab7e3369a84ea7ddf067b670bf5f
gosehwan <kally81@nate.com>
2016-02-10 22:55:58 +0900

    데이터베이스 연동 로직 추

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 var express = require('express');
-
-
-//var pg = require('pg');
-////var conString = "postgres://ixhgduhkaujyyy:PuGdNCVuDhnN6mLPgw5DJSES8V@ec2-54-83-36-203.compute-1.amazonaws.com:5432/d20skifon54bh6";
-////var client = new pg.Client(conString);
-//
-//var client = new pg.Client({
-//	user: "ixhgduhkaujyyy",
-//	password: "PuGdNCVuDhnN6mLPgw5DJSES8V",
-//	database: "d20skifon54bh6",
-//	port: 5432,
-//	host: "ec2-54-83-36-203.compute-1.amazonaws.com",
-//	ssl: true
-//});
-//client.connect();
-
-//var pg = require('pg');
-//var conString = "postgres://ixhgduhkaujyyy:PuGdNCVuDhnN6mLPgw5DJSES8V@ec2-54-83-36-203.compute-1.amazonaws.com:5432/d20skifon54bh6";
-//
-////this initializes a connection pool
-////it will keep idle connections open for a (configurable) 30 seconds
-////and set a limit of 20 (also configurable)
-//pg.connect(conString, function(err, client, done) {
-//  if(err) {
-//    return console.error('error fetching client from pool', err);
-//  }
-//  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-//    //call `done()` to release the client back to the pool
-//    done();
-//
-//    if(err) {
-//      return console.error('error running query', err);
-//    }
-//    console.log(result.rows[0].number);
-//    //output: 1
-//  });
-//});
-
-
-
-
-
-var app = express();
 var pg = require('pg');
+var app = express();
+
 app.set('port', (process.env.PORT || 5000));
 console.log('Node app is running on port', app.get('port'));
 app.use(express.static(__dirname + '/public'));
@@ -52,28 +11,34 @@ app.use(express.static(__dirname + '/bower_components/'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var conString = "postgres://purpleocean:tpghks81@localhost:5432/postgres";
+var client = new pg.Client(conString);
+client.connect();
+
+var query = client.query("SELECT * FROM public.\"Test_table\";");
+query.on("row", function (row, result) {
+    result.addRow(row);
+    //console.log("result : " + result);
+});
+
+
+var resArray;
+query.on("end", function (result) {
+    //console.log(JSON.stringify(result.rows, null, "    "));
+    resArray = result;
+    client.end();
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
 app.get('/index', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		  if (err){
-			  return console.error('error fetching client from pool', err);
-			  //throw err;
-		  }
-		  console.log('Connected to postgres! Getting schemas...');
-
-		  client
-		    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-		    .on('row', function(row) {
-		      console.log(JSON.stringify(row));
-		    });
-		});
-	
-	  response.render('pages/test');
+	console.log(JSON.stringify(resArray.rows));
+		  response.render('pages/test', {"result" : JSON.stringify(resArray.rows)}, function(err, html) {
+		  response.send(html);
+	  });
 });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
