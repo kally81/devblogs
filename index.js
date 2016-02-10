995dbfaf4f87e0cb6781f96c4c73fe688143a3f2
gosehwan <kally81@nate.com>
2016-02-10 11:09:04 +0900

    데이터베이스 관련 소스 수

--- index.js
+++ index.js
@@ -37,21 +37,6 @@ var express = require('express');
 //  });
 //});
 
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err){
-	  return console.error('error fetching client from pool', err);
-	  //throw err;
-  }
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -70,6 +55,23 @@ app.get('/', function(request, response) {
 app.get('/index', function(request, response) {
 	  response.render('pages/test');
 });
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err){
+	  return console.error('error fetching client from pool', err);
+	  //throw err;
+  }
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
